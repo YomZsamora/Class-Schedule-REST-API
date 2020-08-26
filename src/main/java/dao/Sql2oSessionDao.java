22c8927ff22550f539394d6d97c3ba896d5d2efd
Ben Ruwel <benruwel21@gmail.com>
2020-08-26 17:11:08 +0300

    Add methods to create and get all sessions in Sql2oSessionDao class

--- src/main/java/dao/Sql2oSessionDao.java
+++ src/main/java/dao/Sql2oSessionDao.java
@@ -1,7 +1,9 @@
 package dao;
 
 import models.Sessions;
+import org.sql2o.Connection;
 import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
 
 import java.util.List;
 
@@ -15,12 +17,27 @@ public class Sql2oSessionDao implements SessionDao{
 
     @Override
     public void createSession(Sessions session) {
+        String sqlString = "INSERT INTO sessions (session_name, description, cohort_id, module_id, start_time, end_time) VALUES " +
+                "(:session_name, :description, :cohort_id, :module_id, :start_time, :end_time)";
+
+        try(Connection con = sql2o.open()){
+            int id = (int) con.createQuery(sqlString, true)
+                    .bind(session)
+                    .executeUpdate()
+                    .getKey();
+            session.setId(id);
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
 
     }
 
     @Override
     public List<Sessions> getAll() {
-        return null;
+        try(Connection con = sql2o.open()){
+            return con.createQuery("SELECT * FROM sessions")
+                    .executeAndFetch(Sessions.class);
+        }
     }
 
     @Override
