ad1b026494181d70f16d595f2ee2acf7d91fffbf
Ben Ruwel <benruwel21@gmail.com>
2020-08-26 21:55:24 +0300

    Add interface methods in StudentDao class

---
+++ src/main/java/dao/StudentDao.java
@@ -0,0 +1,18 @@
+package dao;
+
+import models.Students;
+
+import java.util.List;
+
+public interface StudentDao {
+
+    //student creates an account
+    void createStudentAccount(Students student);
+    //get all students
+    List<Students> getAll();
+    //get student by id
+    Students findById(int id);
+    //delete student account
+    void deleteById(int id);
+    void clearAll();
+}
