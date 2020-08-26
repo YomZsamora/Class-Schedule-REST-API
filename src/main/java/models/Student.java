5b8360850a0d9d246121609fe42f893eb4fe83c3
Gideon Okuro <gideonollonde@gmail.com>
2020-08-26 06:30:10 +0300

    pass studentid getter method

--- src/main/java/models/Student.java
+++ src/main/java/models/Student.java
@@ -3,9 +3,11 @@ package models;
 public class Student {
     private final String name;
     private final int sessionId;
+    private final int studentId;
 
-    public Student(String name, int sessionsId) {
+    public Student(String name, int studentId, int sessionsId) {
         this.name = name;
+        this.studentId = studentId;
         this.sessionId = sessionsId;
     }
 
@@ -15,5 +17,9 @@ public class Student {
 
     public int getSessionId() {
         return sessionId;
+    }
+
+    public int getStudentId() {
+        return studentId;
     }
 }
--- src/test/java/models/StudentTest.java
+++ src/test/java/models/StudentTest.java
@@ -1,6 +1,5 @@
 package models;
 
-import junit.framework.TestCase;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -8,19 +7,25 @@ import static org.junit.Assert.assertEquals;
 public class StudentTest {
     @Test
     public void studentInstantiatesCorrectly() {
-        Student student = new Student ("Gideon", 1);
+        Student student = new Student ("Gideon", 1, 1);
         assertEquals(true, student instanceof Student);
     }
 
     @Test
     public void getStudentName() {
-        Student student = new Student ("Gideon", 1);
+        Student student = new Student ("Gideon", 1,1);
         assertEquals("Gideon", student.getName());
     }
 
     @Test
+    public void getStudentId() {
+        Student student = new Student ("Gideon", 1, 1);
+        assertEquals(1, student.getStudentId());
+    }
+
+    @Test
     public void getSessionId() {
-        Student student = new Student ("Gideon", 1);
+        Student student = new Student ("Gideon", 1, 1);
         assertEquals(1, student.getSessionId());
     }
 }
