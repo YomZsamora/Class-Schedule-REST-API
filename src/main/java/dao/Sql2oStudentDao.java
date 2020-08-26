694a3623e54526c819a2d90ce7ee59c53d2ae928
Ben Ruwel <benruwel21@gmail.com>
2020-08-26 21:59:58 +0300

    Generate POJO for Sql2oStudentDao class

--- src/main/java/dao/Sql2oStudentDao.java
+++ src/main/java/dao/Sql2oStudentDao.java
@@ -1,4 +1,40 @@
 package dao;
 
-public class Sql2oStudentDao {
+import models.Students;
+import org.sql2o.Sql2o;
+
+import java.util.List;
+
+public class Sql2oStudentDao implements StudentDao {
+
+    private final Sql2o sql2o;
+
+    public Sql2oStudentDao(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    public void createStudentAccount(Students student) {
+
+    }
+
+    @Override
+    public List<Students> getAll() {
+        return null;
+    }
+
+    @Override
+    public Students findById(int id) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+
+    @Override
+    public void clearAll() {
+
+    }
 }
