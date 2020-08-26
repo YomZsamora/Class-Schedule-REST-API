0293555ec250649302c831f0b18f4969833d1cf0
Ben Ruwel <benruwel21@gmail.com>
2020-08-26 16:27:16 +0300

    Implement SessionDao methods in Sql2oSessionDao class

--- src/main/java/dao/Sql2oSessionDao.java
+++ src/main/java/dao/Sql2oSessionDao.java
@@ -1,12 +1,40 @@
 package dao;
 
+import models.Sessions;
 import org.sql2o.Sql2o;
 
-public class Sql2oSessionDao{
+import java.util.List;
+
+public class Sql2oSessionDao implements SessionDao{
 
     private final Sql2o sql2o;
 
     public Sql2oSessionDao(Sql2o sql2o) {
         this.sql2o = sql2o;
+    }
+
+    @Override
+    public void createSession(Sessions session) {
+
+    }
+
+    @Override
+    public List<Sessions> getAll() {
+        return null;
+    }
+
+    @Override
+    public Sessions findById(int id) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+
+    @Override
+    public void clearAll() {
+
     }
 }
