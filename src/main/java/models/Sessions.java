b6f999a30acd1b16388a06785a7e3a27d126f2d8
Gideon Okuro <gideonollonde@gmail.com>
2020-08-26 05:35:12 +0300

    pass a sessions getter method

--- src/main/java/models/Sessions.java
+++ src/main/java/models/Sessions.java
@@ -2,7 +2,13 @@ package models;
 
 public class Sessions {
 
-    public Sessions(String title, String mentor, String venue) {
+    private String title;
 
+    public Sessions(String title, String mentor, String venue) {
+        this.title = title;
+    }
+
+    public String getSessionsTitle() {
+        return title;
     }
 }
--- src/test/java/models/SessionsTest.java
+++ src/test/java/models/SessionsTest.java
@@ -3,10 +3,19 @@ package models;
 import junit.framework.TestCase;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class SessionsTest {
     @Test
     public void sessionsInstantiatesCorrectly() {
         Sessions sessions = new Sessions("Standup", "Samora", "https://meet.google.com/fxm-xjtr-dxm");
+        assertEquals(true, sessions instanceof Sessions);
+    }
+
+    @Test
+    public void getsSessionsTitle() {
+        Sessions sessions = new Sessions("Standup", "Samora", "https://meet.google.com/fxm-xjtr-dxm");
+        assertEquals("Standup", sessions.getSessionsTitle());
     }
 
 }
