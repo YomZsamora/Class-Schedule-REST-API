8dc3664439c77f4589cc51789fcc694586d44cd3
Ben Ruwel <benruwel21@gmail.com>
2020-08-26 23:09:24 +0300

    Add the basic methods for TechnicalMentorDao interface

---
+++ src/main/java/dao/TechnicalMentorDao.java
@@ -0,0 +1,19 @@
+package dao;
+
+import models.TechnicalMentor;
+
+import java.util.List;
+
+public interface TechnicalMentorDao {
+
+    //create new account
+    void createTechnicalMentorAccount(TechnicalMentor technicalMentor);
+
+    //get a list of all TMs
+    List<TechnicalMentor> getAll();
+    TechnicalMentor findById(int id);
+
+    //delete
+    void deleteById(int id);
+    void clearAll();
+}
