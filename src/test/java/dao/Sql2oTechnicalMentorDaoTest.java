ee346158bd1ee1172a5479af5de7a4d7d585d81f
Ben Ruwel <benruwel21@gmail.com>
2020-08-27 12:49:30 +0300

    Complete adding all test for Sql2oTechnicalMentor class

--- src/test/java/dao/Sql2oTechnicalMentorDaoTest.java
+++ src/test/java/dao/Sql2oTechnicalMentorDaoTest.java
@@ -1,38 +1,78 @@
 package dao;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import models.TechnicalMentor;
+import org.junit.*;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
 
 import static org.junit.Assert.*;
 
 public class Sql2oTechnicalMentorDaoTest {
 
-    @Before
-    public void setUp() throws Exception {
+    private static Connection conn;
+    private static Sql2oTechnicalMentorDao technicalMentorDao;
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        String connectionString = "jdbc:postgresql://localhost:5432/class_schedule_test";
+        Sql2o sql2o = new Sql2o(connectionString, "User", "7181");
+
+        technicalMentorDao = new Sql2oTechnicalMentorDao(sql2o);
+        conn = sql2o.open();
     }
 
     @After
     public void tearDown() throws Exception {
+        System.out.println("Clearing database");
+        technicalMentorDao.clearAll();
+    }
+
+    @AfterClass
+    public static void shutDown() throws Exception {
+        conn.close();
+        System.out.println("Connection closed");
     }
 
     @Test
-    public void createTechnicalMentorAccount() {
+    public void createTechnicalMentorAccountSetsId() {
+        TechnicalMentor testTechnicalMentor = setupTechnicalMentor();
+        assertNotEquals(0, testTechnicalMentor.getId());
     }
 
     @Test
-    public void getAll() {
+    public void getAllReturnsAllTMs() {
+        TechnicalMentor testTechnicalMentor = setupTechnicalMentor();
+        technicalMentorDao.createTechnicalMentorAccount(testTechnicalMentor);
+        assertEquals(1, technicalMentorDao.getAll().size());
     }
 
     @Test
-    public void findById() {
+    public void findByIdReturnsCorrectResult() {
+        TechnicalMentor testTechnicalMentor = setupTechnicalMentor();
+        TechnicalMentor otherTechnicalMentor = setupTechnicalMentor();
+        assertEquals(otherTechnicalMentor,technicalMentorDao.findById(otherTechnicalMentor.getId()));
     }
 
     @Test
-    public void deleteById() {
+    public void deleteByIdRemovesCorrectTM() {
+        TechnicalMentor testTechnicalMentor = setupTechnicalMentor();
+        TechnicalMentor otherTechnicalMentor = setupTechnicalMentor();
+        technicalMentorDao.deleteById(otherTechnicalMentor.getId());
+        assertEquals(otherTechnicalMentor, technicalMentorDao.findById(otherTechnicalMentor.getId()));
     }
 
     @Test
-    public void clearAll() {
+    public void clearAllRemovesAllRecords() {
+        TechnicalMentor testTechnicalMentor = setupTechnicalMentor();
+        TechnicalMentor otherTechnicalMentor = setupTechnicalMentor();
+        technicalMentorDao.deleteById(otherTechnicalMentor.getId());
+        assertEquals(otherTechnicalMentor, technicalMentorDao.findById(otherTechnicalMentor.getId()));
+    }
+
+    //helper methods
+    private TechnicalMentor setupTechnicalMentor(){
+        TechnicalMentor technicalMentor = new TechnicalMentor("Samora", "7hi687bh8shf9780u");
+        technicalMentorDao.createTechnicalMentorAccount(technicalMentor);
+        return technicalMentor;
     }
 }
