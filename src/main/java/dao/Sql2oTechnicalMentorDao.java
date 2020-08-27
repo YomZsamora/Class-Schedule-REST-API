1091aecb8cb6bd5900afce61e285c82447fc9ef5
Ben Ruwel <benruwel21@gmail.com>
2020-08-27 11:45:56 +0300

    Implement TechnicalMentorDao methods in Sql2oTechnicalMentor class

--- src/main/java/dao/Sql2oTechnicalMentorDao.java
+++ src/main/java/dao/Sql2oTechnicalMentorDao.java
@@ -1,4 +1,40 @@
 package dao;
 
-public class Sql2oTechnicalMentorDao {
+import models.TechnicalMentor;
+import org.sql2o.Sql2o;
+
+import java.util.List;
+
+public class Sql2oTechnicalMentorDao implements TechnicalMentorDao {
+
+    private final Sql2o sql2o;
+
+    public Sql2oTechnicalMentorDao(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    public void createTechnicalMentorAccount(TechnicalMentor technicalMentor) {
+
+    }
+
+    @Override
+    public List<TechnicalMentor> getAll() {
+        return null;
+    }
+
+    @Override
+    public TechnicalMentor findById(int id) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+
+    @Override
+    public void clearAll() {
+
+    }
 }
