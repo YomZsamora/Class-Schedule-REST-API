1acb1317ccbf1f1c51f0bc0d11def942953330fc
Gideon Okuro <gideonollonde@gmail.com>
2020-08-28 00:08:50 +0300

    add a getter and setter method for content

--- src/main/java/models/Comments.java
+++ src/main/java/models/Comments.java
@@ -1,10 +1,12 @@
 package models;
 
 public class Comments {
+    private String content;
     private int student_id;
 
     public Comments(int student_id, String content) {
         this.student_id = student_id;
+        this.content = content;
     }
 
     public int getStudentId() {
@@ -13,5 +15,13 @@ public class Comments {
 
     public void setStudentId(int student_id) {
         this.student_id = student_id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
     }
 }
--- src/test/java/models/CommentsTest.java
+++ src/test/java/models/CommentsTest.java
@@ -25,4 +25,17 @@ public class CommentsTest {
         comments.setStudentId(1);
         assertEquals(1, comments.getStudentId());
     }
+
+    @Test
+    public void getContent() {
+        Comments comments = new Comments(1, "Session was well coordinated");
+        assertEquals("Session was well coordinated", comments.getContent());
+    }
+
+    @Test
+    public void setContent() {
+        Comments comments = new Comments(1, "Session was well coordinated");
+        comments.setContent("Session was well coordinated");
+        assertEquals("Session was well coordinated", comments.getContent());
+    }
 }
