728b6f97f5613182ba41b483ade59e4e71446f6c
Ben Ruwel <benruwel21@gmail.com>
2020-08-28 15:31:31 +0300

    Add POJO for Cohort class model

--- src/main/java/models/Cohort.java
+++ src/main/java/models/Cohort.java
@@ -1,5 +1,48 @@
 package models;
 
+import java.util.Date;
+
 public class Cohort {
 
+    private String name;
+    private Date start_date;
+
+    public Cohort(String name, Date start_date) {
+        this.name = name;
+        this.start_date = start_date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(Date start_date) {
+        this.start_date = start_date;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Cohort cohort = (Cohort) o;
+
+        if (!name.equals(cohort.name)) return false;
+        return start_date.equals(cohort.start_date);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + start_date.hashCode();
+        return result;
+    }
 }
