38fc6ccdef8a65c9c0a3b44e3b5f46db05daa8f6
Ben Ruwel <benruwel21@gmail.com>
2020-08-28 15:56:21 +0300

    Add passing tests for Cohort model class

--- src/test/java/models/CohortTest.java
+++ src/test/java/models/CohortTest.java
@@ -5,26 +5,19 @@ import org.junit.Test;
 import java.sql.Timestamp;
 import java.util.Date;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class CohortTest {
     @Test
     public void cohortInstantiatesCorrectly() {
-//        Cohort cohort = setUpCohort();
-        assertTrue(true);
+        Cohort cohort = setUpCohort();
+        assertEquals(true, cohort instanceof Cohort);
     }
 
-    //Revisit Date
-//    private Cohort setUpCohort() {
-//        Date start_date = new Date(System.currentTimeMillis());
-//        try{
-//            //we make our thread 'sleep' to simulate different start and end times
-//            Thread.sleep(1000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//        Timestamp end_time = new Timestamp(System.currentTimeMillis());
-//        Cohort cohort = new Cohort("MC30", start_date);
-//        return Cohort;
-//    }
+    //helpers
+    private Cohort setUpCohort() {
+        Date start_date = new Date();
+        return new Cohort("MC30", start_date);
+    }
 }
