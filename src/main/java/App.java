03f85941b4d6d229455e3c22b9332d2c0891386b
Ben Ruwel <benruwel21@gmail.com>
2020-08-31 15:14:21 +0300

    Add method in App.java to allow for CORS

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -275,5 +275,25 @@ public class App {
         after((request, response) -> {
             response.type("application/json");
         });
+
+        //this allows Cross-Origin Resource Sharing (CORS policy) between different servers
+        options("/*",
+                (request, response) -> {
+                    String accessControlRequestHeaders = request
+                            .headers("Access-Control-Request-Headers");
+                    if (accessControlRequestHeaders != null) {
+                        response.header("Access-Control-Allow-Headers",
+                                accessControlRequestHeaders);
+                    }
+                    String accessControlRequestMethod = request
+                            .headers("Access-Control-Request-Method");
+                    if (accessControlRequestMethod != null) {
+                        response.header("Access-Control-Allow-Methods",
+                                accessControlRequestMethod);
+                    }
+                    return "OK";
+                });
+        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+
     }
 }
--- src/test/java/dao/Sql2oModuleDaoTest.java
+++ src/test/java/dao/Sql2oModuleDaoTest.java
@@ -20,8 +20,8 @@ public class Sql2oModuleDaoTest {
 //        Sql2o sql2o = new Sql2o(connectionString, "gideon", "33450715");
         //Sql2o sql2o = new Sql2o(connectionString, "User", "7181");
 
-        String connectionString = "jdbc:postgres://ec2-52-70-15-120.compute-1.amazonaws.com:5432/dc3s96ls8kqvhf";
-        Sql2o sql2o = new Sql2o(connectionString, "bdmoxcpjclbybe", "1721f547f35ba2a92f727c64d258d4e09b8466ec13618191633451f0e958faa3");
+        String connectionString = "jdbc:postgres://ec2-52-200-111-186.compute-1.amazonaws.com:5432/d9dbmvjbe8ikml";
+        Sql2o sql2o = new Sql2o(connectionString, "mwstdukciefqhd", "077938e0cb68bfccc22eb8b19804f13757c2fbd4876103c77b525b70e499f110");
 
         moduleDao = new Sql2oModuleDao(sql2o);
         conn = sql2o.open();
