b71e5eff20b36ace3143a1b63c9506f556d43f44
Ben Ruwel <benruwel21@gmail.com>
2020-08-31 14:43:55 +0300

    Add heroku cofig vars in all Dao files and App.java

--- src/test/java/dao/Sql2oStudentDaoTest.java
+++ src/test/java/dao/Sql2oStudentDaoTest.java
@@ -14,9 +14,12 @@ public class Sql2oStudentDaoTest {
 
     @BeforeClass
     public static void setUp() throws Exception {
-        String connectionString = "jdbc:postgresql://localhost:5432/class_schedule_test";
 //        Sql2o sql2o = new Sql2o(connectionString, "gideon", "33450715");
-        Sql2o sql2o = new Sql2o(connectionString, "User", "7181");
+//        String connectionString = "jdbc:postgresql://localhost:5432/class_schedule_test";
+//        Sql2o sql2o = new Sql2o(connectionString, "User", "7181");
+
+        String connectionString = "jdbc:postgres://ec2-52-70-15-120.compute-1.amazonaws.com:5432/dc3s96ls8kqvhf";
+        Sql2o sql2o = new Sql2o(connectionString, "bdmoxcpjclbybe", "1721f547f35ba2a92f727c64d258d4e09b8466ec13618191633451f0e958faa3");
 
 
         studentDao = new Sql2oStudentDao(sql2o);
